057c4da15e438aac563614f3e2480db193de0a9f
Tnarita0000 <naritatakuya0000@gmail.com>
2018-03-20 00:24:34 +0900

    change publid dir & frontend dir setting

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.toObservable
-import spark.kotlin.*
+//import spark.kotlin.*
 import spark.Spark.*
 import spark.ModelAndView
 import spark.template.jade.JadeTemplateEngine
@@ -9,13 +9,24 @@ class Main {
   companion object {
     @JvmStatic fun main(args: Array<String>) {
       setPort(3000)
-      staticFileLocation("/public")
-      val map: Map<String, String> = hashMapOf("message" to "Hello World!")
+      this.setPublicDir()
+      this.setFrontendDir()
 
+      val map: Map<String, String> = hashMapOf("message" to "Hello World!")
       get("/", { rq, rs -> 
         println(rs)
         ModelAndView(map, "index")
       }, JadeTemplateEngine())
+    }
+
+    fun setPublicDir() {
+      staticFileLocation("/public")
+    }
+
+    fun setFrontendDir() {
+      val projectDir = System.getProperty("user.dir")
+      val staticDir = "/src/main/frontend/dist"
+      staticFiles.externalLocation(projectDir + staticDir)
     }
   }
 }
